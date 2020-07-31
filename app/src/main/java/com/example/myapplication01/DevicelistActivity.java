ca21c5e93a0e716cca6bc23607da40b398bc10d9
Nguyễn Việt Tiệp <viettiep0412@gmail.com>
2020-08-01 05:01:56 +0700

    05.01a.m 01082020

--- app/src/main/java/com/example/myapplication01/DevicelistActivity.java
+++ app/src/main/java/com/example/myapplication01/DevicelistActivity.java
@@ -6,6 +6,9 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import jp.co.melco.mxcomponent.MELMxCommunication;
+import jp.co.melco.mxcomponent.MELMxLabel;
+import jp.co.melco.mxcomponent.MELMxOpenSettings;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
