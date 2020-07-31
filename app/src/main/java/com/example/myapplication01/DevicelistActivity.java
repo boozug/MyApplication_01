fca65ddaa2cb30976172c339514fb79394880283
Nguyễn Việt Tiệp <viettiep0412@gmail.com>
2020-08-01 04:11:34 +0700

    04.41a.m 01082020

--- app/src/main/java/com/example/myapplication01/DevicelistActivity.java
+++ app/src/main/java/com/example/myapplication01/DevicelistActivity.java
@@ -16,8 +16,8 @@ public class DevicelistActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_devicelist);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
