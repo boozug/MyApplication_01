733702e93af7b1051be33931c01fd1078085eeec
Nguyễn Việt Tiệp <viettiep0412@gmail.com>
2020-08-09 19:06:15 +0700

    07.06p.m 09082020

--- app/src/main/java/com/example/myapplication01/Devicelist_Activity_main.java
+++ app/src/main/java/com/example/myapplication01/Devicelist_Activity_main.java
@@ -1,13 +1,15 @@
 package com.example.myapplication01;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+import androidx.recyclerview.widget.ItemTouchHelper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.view.View;
 
@@ -19,6 +21,8 @@ public class Devicelist_Activity_main extends AppCompatActivity {
     Devicelist_deviceAdapter deviceAdapter;
     ArrayList<Addevice_UDT_activity> addeviceUDTactivityArrayList = new ArrayList<>();
     FloatingActionButton actionButton;
+    SwipeController swipeController = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +31,10 @@ public class Devicelist_Activity_main extends AppCompatActivity {
         addeviceUDTactivityArrayList = Addevice_DBUserAdapter.get_all_devices();
         deviceAdapter = new Devicelist_deviceAdapter(addeviceUDTactivityArrayList);
         actionButton = (FloatingActionButton)findViewById(R.id.fab);
-        recyclerView = findViewById(R.id.recycler_view);recyclerView.addItemDecoration(new DividerItemDecoration(this,  LinearLayoutManager.VERTICAL));
-        LinearLayoutManager layoutManager = new   LinearLayoutManager(this);
+        recyclerView = findViewById(R.id.recycler_view);
+//        recyclerView.addItemDecoration(new DividerItemDecoration(this,  LinearLayoutManager.VERTICAL));
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(deviceAdapter);
         actionButton.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +46,24 @@ public class Devicelist_Activity_main extends AppCompatActivity {
             private void show_insertactivity() {
                 Intent intent = new Intent(Devicelist_Activity_main.this, Adddevice_activity_1.class);
                 startActivity(intent);
+            }
+        });
+
+        ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeController);
+        itemTouchhelper.attachToRecyclerView(recyclerView);
+
+        swipeController = new SwipeController(new Try_swipeControlleraction() {
+            public void onRightClicked(int position) {
+                deviceAdapter.list_device.remove(position);
+                deviceAdapter.notifyItemRemoved(position);
+                deviceAdapter.notifyItemRangeChanged(position, deviceAdapter.getItemCount());
+            }
+        });
+
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+                swipeController.onDraw(c);
             }
         });
     }
---
+++ app/src/main/java/com/example/myapplication01/SwipeController.java
@@ -0,0 +1,87 @@
+package com.example.myapplication01;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+enum ButtonsState {
+    GONE,
+    LEFT_VISIBLE,
+    RIGHT_VISIBLE
+}
+public class SwipeController extends ItemTouchHelper.Callback {
+    private Try_swipeControlleraction buttonsActions = null;
+    int LEFT = ItemTouchHelper.LEFT;
+    int RIGHT = ItemTouchHelper.RIGHT ;
+    private boolean swipeback = false;
+    private static final int buttonWidth = 300;
+    private RectF buttonInstance = null;
+    private ButtonsState buttonShowedState = ButtonsState.GONE;
+    private RecyclerView.ViewHolder currentItemViewHolder = null;
+
+    public SwipeController(Try_swipeControlleraction buttonsActions) {
+        // final la khong the ghep gia tri do vao: dang constant.
+        this.buttonsActions = buttonsActions;
+    }
+    @Override
+    public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+        return makeMovementFlags(0, LEFT | RIGHT);    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+    }
+
+    private void drawButtons(Canvas c, RecyclerView.ViewHolder viewHolder) {
+        float buttonWidthWithoutPadding = buttonWidth - 20;
+        float corners = 16;
+
+        View itemView = viewHolder.itemView;
+        Paint p = new Paint();
+
+        RectF leftButton = new RectF(itemView.getLeft(), itemView.getTop(), itemView.getLeft() + buttonWidthWithoutPadding, itemView.getBottom());
+        p.setColor(Color.BLUE);
+        c.drawRoundRect(leftButton, corners, corners, p);
+        drawText("EDIT", c, leftButton, p);
+
+        RectF rightButton = new RectF(itemView.getRight() - buttonWidthWithoutPadding, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+        p.setColor(Color.RED);
+        c.drawRoundRect(rightButton, corners, corners, p);
+        drawText("DELETE", c, rightButton, p);
+
+        buttonInstance = null;
+        if (buttonShowedState == ButtonsState.LEFT_VISIBLE) {
+            buttonInstance = leftButton;
+        }
+        else if (buttonShowedState == ButtonsState.RIGHT_VISIBLE) {
+            buttonInstance = rightButton;
+        }
+    }
+
+    private void drawText(String text, Canvas c, RectF button, Paint p) {
+        float textSize = 60;
+        p.setColor(Color.WHITE);
+        p.setAntiAlias(true);
+        p.setTextSize(textSize);
+
+        float textWidth = p.measureText(text);
+        c.drawText(text, button.centerX()-(textWidth/2), button.centerY()+(textSize/2), p);
+    }
+
+    public void onDraw(Canvas c) {
+        if (currentItemViewHolder != null) {
+            drawButtons(c, currentItemViewHolder);
+        }
+    }
+}
