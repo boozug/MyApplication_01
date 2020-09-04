52c3d1008112e142dc2e9b61d0976bc355773429
Nguyễn Việt Tiệp <nguyenviet.tiep@mevn.com.vn>
2020-09-04 16:36:02 +0700

    0434pm 04092020-Add author name

--- app/src/main/java/com/example/myapplication01/Devices_interface_activity/fragments/Monitordevices/VIew/ReaddeviceAdapter.java
+++ app/src/main/java/com/example/myapplication01/Devices_interface_activity/fragments/Monitordevices/VIew/ReaddeviceAdapter.java
@@ -1,8 +1,12 @@
 package com.example.myapplication01.Devices_interface_activity.fragments.Monitordevices.VIew;
 
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageSwitcher;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,15 +26,24 @@ public class ReaddeviceAdapter extends RecyclerView.Adapter<ReaddeviceAdapter.Vi
     // Provide a reference to the views for each data item
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView id, device_name,address,value;
-
+        Button notificator_btn;
         public ViewHolder(View view) {
             super(view);
             id = view.findViewById(R.id.id_textView);
             device_name = view.findViewById(R.id.devicename_textView);
             address = view.findViewById(R.id.address_textView);
             value = view.findViewById(R.id.value_textView);
+            notificator_btn = view.findViewById(R.id.button_notification1);
+            notificator_btn.setOnClickListener(new View.OnClickListener() {
+                private static final String TAG = "Read_device_adapter";
+                @Override
+                public void onClick(View view) {
+                    Log.d(TAG, "Element " + getAdapterPosition() + " clicked.");
+                }
+            });
         }
     }
+
     // Provide a suitable constructor (depends on the kind of data set)
     public ReaddeviceAdapter(List<Read_device_type> readDeviceTypeList){
         this.read_device_typeList = readDeviceTypeList;
@@ -48,10 +61,10 @@ public class ReaddeviceAdapter extends RecyclerView.Adapter<ReaddeviceAdapter.Vi
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         Read_device_type read_device_type_tmp = read_device_typeList.get(position);
-        holder.id.setText(String.valueOf(read_device_type_tmp.getId()));
-        holder.device_name.setText(read_device_type_tmp.getDevice_name());
-        holder.address.setText(read_device_type_tmp.getAddress());
-        holder.value.setText(String.valueOf(read_device_type_tmp.getValue()));
+        holder.id.setText("Address ID: "+String.valueOf(read_device_type_tmp.getId()));
+        holder.device_name.setText("Device type: "+read_device_type_tmp.getDevice_name());
+        holder.address.setText("Address number: " + read_device_type_tmp.getAddress());
+        holder.value.setText("Value is: "+ String.valueOf(read_device_type_tmp.getValue()));
     }
     // Return the size of your data set (invoked by the layout manager)
     @Override
